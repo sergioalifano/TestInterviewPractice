8b0e529bbb4e7aaf6b67a6de4e213884cd123dca
Eric Fowler <ericfowler303@gmail.com>
2015-03-10 22:35:06 -0600

    Initial Commit
    content comes from exercism.io

---
+++ Bob/Bob/Bob.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Bob
+{
+    public string Hey(string bobsWords)
+    {
+        return string.Empty; // Replace this
+    }
+}
---
+++ Leap/Leap/Year.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Year
+{
+    public static bool IsLeap(int year)
+    {
+        return false; // Replace this
+    }
+}
+
