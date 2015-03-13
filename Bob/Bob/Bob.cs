b6d24bb0c61f2dec78f5482b3c03a2933c9f406d
Sergio Alifano <sergio.alifano@gmail.com>
2015-03-13 09:40:53 -0600

    Final

--- Bob/Bob/Bob.cs
+++ Bob/Bob/Bob.cs
@@ -4,10 +4,67 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-class Bob
+class Program
 {
-    public string Hey(string bobsWords)
+    static void Main(String[] args)
     {
-        return string.Empty; // Replace this
+        Bob bob = new Bob();
+       
+        Console.WriteLine( bob.Hey("1,2"));
+
+        Console.ReadKey();
     }
 }
+
+class Bob
+{
+   
+    public string Hey(string bobsWords)
+    {
+        //check for silence
+        bool allSpaces = true;
+        foreach (Char c in bobsWords)
+        {
+            if (!Char.IsWhiteSpace(c))
+            {
+                allSpaces = false;
+                break;
+            }              
+        }
+        if (allSpaces || bobsWords.Length == 0)
+        {
+            return "Fine. Be that way!";
+        }
+
+        //chech for yelling string (only char uppercase)
+        bool stringIsUpper = true;
+        foreach (Char c in bobsWords)
+        {
+            if (!Char.IsUpper(c))
+            {
+                if (Char.IsLetter(c) || Char.IsDigit(c))
+                {
+                    stringIsUpper = false;
+                    break;
+                }               
+            }
+        }
+
+        if (stringIsUpper)
+            return "Whoa, chill out!";
+
+        //check if it's a question
+        if (bobsWords.Contains("?") && (bobsWords.IndexOf('?')==bobsWords.Length-1))
+        {
+            return "Sure.";
+        }
+
+
+
+        return "Whatever.";
+
+    }
+}
+
+
+
--- Leap/Leap/Year.cs
+++ Leap/Leap/Year.cs
@@ -4,10 +4,37 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+class Program
+{
+    static void Main(String[] args)
+    {
+        Console.WriteLine(Year.IsLeap(20));
+
+        Console.ReadKey();
+    }
+}
+
+//The tricky thing here is that a leap year occurs:
+//on every year that is evenly divisible by 4
+//  except every year that is evenly divisible by 100
+//    unless the year is also evenly divisible by 400
 class Year
 {
     public static bool IsLeap(int year)
     {
+        if (year % 4 == 0)
+        {
+            if (year % 100 == 0 && (year % 400==0))
+            {
+                return true;
+            }
+            if (year % 100 == 0)
+            {
+                return false;
+            }
+            else return true;
+        }
+
         return false; // Replace this
     }
 }
